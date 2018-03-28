a0a81b6d132ebc31a38a3d17a99ec36720b55776
Vladimir Kotal <vlada@devnull.cz>
2018-03-28 21:51:51 +0200

    better formatting

--- list.py
+++ list.py
@@ -16,6 +16,31 @@ from util import Util
 TOKEN_ENV_VAR = "DROPBOX_TOKEN"
 TOKEN = os.getenv(TOKEN_ENV_VAR)
 
+
+def print_entry(entry_type, name, name_indent, size):
+    print("{} {:{width}} {}".
+          format(entry_type, name, size, width=name_indent))
+
+
+def list_entries(logger, dirs, files):
+    """
+    Print list of directories and files along with sizes (if any).
+    Directories first. Both sorted according to their name.
+    """
+    # Determine longest name first.
+    maxlen = 0
+    for item in list(dirs.keys()) + list(files.keys()):
+        if len(item) > maxlen:
+            maxlen = len(item)
+    logger.debug("maxlen = {}".format(maxlen))
+
+    for item in sorted(dirs.keys()):
+        print_entry('-', item, maxlen, dirs[item])
+
+    for item in sorted(files.keys()):
+        print_entry('d', item, maxlen, files[item])
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='List Dropbox folder')
 
@@ -41,17 +66,19 @@ if __name__ == "__main__":
 
     res = dbox.list_folder(dbx, logger, args.folder, '')
     logger.debug(res)
+    dirs = {}
+    files = {}
     for key in res.keys():
         metadata = res[key]
         size = ''
+        logger.debug(key)
         if isinstance(metadata, dropbox.files.FolderMetadata):
-            entry_type = 'd'
+            dirs[key] = size
         elif isinstance(metadata, dropbox.files.FileMetadata):
-            entry_type = '-'
             size = size if args.numeric else Util.bytecnt_to_str(metadata.size)
+            files[key] = size
         else:
             logger.error("Unknown type of {}".format(key))
             entry_type = 'N/A'
 
-        print("{} {} {}".
-              format(entry_type, size, key))
+    list_entries(logger, dirs, files)
